aa853bb12636ad0ba0884c65b9ea6600abb8871d
zzy <Xiao.ye_Zhang@qq.com>
2022-05-08 22:34:43 +0800

    SpringCloud(third)

---
+++ eureka-server/src/main/java/cn/itcast/eureka/EurekaApplication.java
@@ -0,0 +1,19 @@
+package cn.itcast.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 星河、
+ * @version 1.0
+ * @introduction 努力总不会错的对吧，宝~
+ */
+@SuppressWarnings({"all"})
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -3,6 +3,7 @@ package cn.itcast.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -21,6 +22,7 @@ public class OrderApplication {
      * 返回RestTemplate 并注入 Spring 容器
      * */
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
